ad7e664fbe21ef4e3207b700ba6293d0897f72f4
batacraft <lordhunter017@gmail.com>
2024-08-04 07:06:50 +0545

    add

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from 'react'
-import NavBar from '../Components/Navbar/NavBar'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import NavBar from '../Components/Navbar/NavBar';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Loading } from '../Components/Loading';
-// import DisplayProduct from '../Components/Card/DisplayProduct'
 import FooterCardDisplay from '../Components/Card/FooterCardDisplay';
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-
-  const params = useParams()
-
-  const [singleProduct, setSingleprd] = useState({}); 
+  const params = useParams();
+  const [singleProduct, setSingleprd] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const id = params.productId;
@@ -21,68 +18,74 @@ const ProductDetails = () => {
     const fetchProduct = async () => {
       try {
         const res = await axios.get(`https://dummyjson.com/products/${id}`);
-        setSingleprd(res.data); 
+        setSingleprd(res.data);
         setLoading(false);
       } catch (error) {
         setError(error);
         setLoading(false);
       }
-    }
+    };
     fetchProduct();
-  },[id, category])
+  }, [id, category]);
 
   if (loading) {
-    return <div className='w-screen h-screen flex justify-center items-center'><Loading /></div>
+    return <div className='w-screen h-screen flex justify-center items-center'><Loading /></div>;
   }
 
   // if (error) {
   //   return <div>Error: {error.message}</div>;
   // }
 
-  
+  const handleAddCart = () => {
+    const cart = JSON.parse(localStorage.getItem("cartItemsKey")) ?? [];
+    const found = cart.find((item) => item.id === singleProduct.id);
+    if (found) {
+      found.quantity = found.quantity + 1;
+      localStorage.setItem("cartItemsKey", JSON.stringify(cart));
+    } else {
+      cart.push({ ...singleProduct, quantity: 1 });
+      localStorage.setItem("cartItemsKey", JSON.stringify(cart));
+    }
+  };
+
   return (
     <>
-
-     {/* {<p> Product id = {params.productId}</p>} */}
-
       <NavBar />
-     
-     
-  
-    
-        <div className='flex justify-center py-[200px] px-2 w-full h-full '>
+      <div className='flex justify-center py-[200px] px-2 w-full h-full '>
         <div className='w-full flex flex-wrap justify-center items-center gap-10 '>
           <div id="left" className='w-full h-[20rem] md:h-[40rem] lg:w-[40%]'>
-            <img src={singleProduct.images[0]} alt={singleProduct.title} className='w-full h-full object-top object-contain' />
+            {singleProduct.images && singleProduct.images[0] && (
+              <img src={singleProduct.images[0]} alt={singleProduct.title} className='w-full h-full object-top object-contain' />
+            )}
           </div>
           <div id="right" className='lg:w-[40%] h-full'>
             <div id="detail">
               <h3 className='font-bold text-[32px] mt-10'>{singleProduct.title}</h3>
               <span className='font-medium text-[22px] text-gray-800 pt-8'>${singleProduct.price}</span>
-              <div class="flex items-center mt-2.5 mb-5">
-                <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
+              <div className="flex items-center mt-2.5 mb-5">
+                <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                  <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
+                  <svg className="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                  </svg>
                 </div>
-                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{singleProduct.rating}</span>
-            </div>
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{singleProduct.rating}</span>
+              </div>
               <p className='font-normal text-[20px] py-4'>{singleProduct.description}</p>
             </div>
             <div className='mt-10'>
-              <Link className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <Link className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleAddCart}>
                 Add to cart
               </Link>
             </div>
@@ -90,12 +93,11 @@ const ProductDetails = () => {
         </div>
       </div>
 
-    
       <div className='pt-10'>
         <FooterCardDisplay />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
